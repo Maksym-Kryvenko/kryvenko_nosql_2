2b8f6828cba76376947444d68d480aad5b2b6952
Maksym Kryvenko <maks1997ym@gmail.com>
2026-05-27 20:37:00 +0200

    Load to pinecone

---
+++ scripts/03_load_to_pinecone.py
@@ -0,0 +1,54 @@
+"""
+Create an index in Pinecone, load vectors with metadata
+"""
+
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+
+load_dotenv()
+
+INPUT_PARQUET = "data/arxiv_subset.parquet"
+INPUT_EMBEDDINGS = "embeddings/embeddings.npy"
+INDEX_NAME = "arxiv-papers"
+VECTOR_DIM = 768
+BATCH_SIZE = 200 
+
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+
+if not pc.has_index(INDEX_NAME):
+    pc.create_index_for_model(
+        name=INDEX_NAME,
+        cloud="aws",
+        region="us-east-1"
+    )
+
+data = pd.read_parquet(INPUT_PARQUET)
+embed = np.load(INPUT_EMBEDDINGS)
+
+pinecone_records = []
+for _e, _m in zip(embed, data.to_dict("records")):
+    pinecone_records.append({
+        "id": f"paper_{_m["id"]}",
+        "vector": _e,
+        "arxiv_id": _m["id"],
+        "title": _m["title"],
+        "abstract": _m["abstract"][:250],
+        "authors": _m["authors"][:100],
+        "year": _m["year"],
+        "category": _m["category"]
+    })
+
+def chunks(iterable, batch_size=200):
+    """A helper function to break an iterable into chunks of size batch_size."""
+    it = iter(iterable)
+    chunk = tuple(itertools.islice(it, batch_size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, batch_size))
+
+for ids_vectors_chunk in tqdm(chunks(pinecone_records, batch_size=100)):
+    index.upsert(vectors=ids_vectors_chunk)
